0a4f1402f5e181e93f98c4a3508e1c253d5d4452
c4pid <c4piddd@gmail.com>
2022-01-17 11:22:56 +0700

    static scss

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+// console.log(morgan)
+// morgan.token('type', function (req, res) { return req.method })
+// app.use(morgan(':type'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+
+// console.log(__dirname)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
